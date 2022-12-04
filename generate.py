1e03d878b9ccf163727101263d7b6a73019bc743
Maxime Boussard <sh4rkman@users.noreply.github.com>
2022-12-04 20:18:43 +0100

    sharkman's rework
    Code Cleanup
    Documentation

--- generate.py
+++ generate.py
@@ -7,59 +7,64 @@
 # on procède par tirage aléatoire des maps, puis pour chaque map tirage aléatoire parmis les layers disponibles
 # attention la version actuelle est case sensitive, il faut respecter les majuscules dans les noms de maps!
 
-import sys
-
-try:
-fichier_layer = open(sys.argv[1], 'r')
-except:
-import traceback
-traceback.print_exc()
-sys.exit(1)
-
+import traceback, random
+from datetime import datetime
 from collections import defaultdict
 
+
+inputFile = "source_layers.cfg"
+outputFile = "LayerRotation.cfg"
+nb_layers = 0
 maps = set()
 layers_par_map = defaultdict(list)
-nb_layers = 0
+
+
+
+
+# -----------------------------------
+# OPEN LAYERS SOURCE FILE & LOAD THEM
+# -----------------------------------
+try:
+    fichier_layer = open(inputFile, 'r')
+except:
+    traceback.print_exc()
+
 
 for ligne in fichier_layer.readlines():
-ligne = ligne.strip()
-if not ligne:
-continue
-if ligne.startswith('//'):
-continue
-if ligne.startswith('#'):
-continue
+    ligne = ligne.strip()
 
-map, mode, version = ligne.split('_')
+    if not ligne:
+        continue
+    if ligne.startswith('//') or ligne.startswith('#'):
+        continue
 
-# FIXME case sensitive ?
-maps.add(map)
-nb_layers += 1
-layers_par_map[map].append(ligne)
+    map, mode, version = ligne.split('_')
+    # FIXME case sensitive ?
+    maps.add(map)
+    nb_layers += 1
+    layers_par_map[map].append(ligne)
 
 
-#print("%d maps" % len(maps))
-#print("%d layers" % nb_layers)
-#print(layers_par_map)
 
-import random
+# ---------------------
+# GENERATE MAP ROTATION
+# ---------------------
+
 maps_aleatoire = random.sample(maps, k = len(maps))
-#print(maps_aleatoire)
 
 layers_aleatoires = []
 for map in maps_aleatoire:
-layers_aleatoires.append(random.choice(layers_par_map[map]))
+    layers_aleatoires.append(random.choice(layers_par_map[map]))
 
-#print(layers_aleatoires)
 
-from datetime import datetime
-lignes_sortie = ["// Fichier genere aleatoirement par rotation_random.py le %s" % datetime.now().strftime('%d/%m/%Y a %H:%M'), ]
-lignes_sortie.append("// ne pas modifier manuellement car il sera ecrase au prochain lancement")
-lignes_sortie.append("// fichier source: %s" % sys.argv[1])
-lignes_sortie.append("// %d layers pour %d maps" % (nb_layers, len(maps)))
-lignes_sortie.append('')
-lignes_sortie += layers_aleatoires
-lignes_sortie.append('')
-
-print('\n'.join(lignes_sortie))
+# -------------------------------
+# CREATE/OVERIDE MAPROTATION FILE
+# -------------------------------
+with open(outputFile,"w") as outputFile:
+    outputFile.write("// Fichier genere aleatoirement par rotation_random.py le %s\n" % datetime.now().strftime('%d/%m/%Y a %H:%M'))
+    outputFile.write("// ne pas modifier manuellement car il sera ecrase au prochain lancement\n")
+    outputFile.write("// fichier source: %s\n" % "source_layers.cfg")
+    outputFile.write("// %d layers pour %d maps\n" % (nb_layers, len(maps)))
+    outputFile.write('\n')
+    for layer in layers_aleatoires:
+        outputFile.write(f"{layer}\n")
